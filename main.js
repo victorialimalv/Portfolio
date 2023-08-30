d4507d100d8915c014b4b99709287faa30854338
Victoria Lima <victorialima@Air-de-Victoria.home>
2023-08-30 19:37:41 +0100

    feat: validation form

---
+++ main.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.title', { duration: 2000, rotate: { x: 10, y: 40, z: 0}});
+sr.reveal('.project-principal', { duration: 2000, rotate: { x: 10, y: 40, z: 0}});
+sr.reveal('#my-projects-list', { duration: 2000, rotate: { x: 10, y: 40, z: 0}});
+sr.reveal('.home', { duration: 3000, rotate: { x: 0, y: 20, z: 0}});
